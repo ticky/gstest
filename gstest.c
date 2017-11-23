be0a0ab60ad4a15a3223aa4a801287baac59e895
Jessica Stokes <hello@jessicastokes.net>
2017-11-23 22:44:25 +1100

    Okay we need all three of these to colour the screen

--- gstest.c
+++ gstest.c
@@ -21,9 +21,9 @@ int main(int argc, char *argv[]) {
   // Initialise the DMAKit's channel
   dmaKit_chan_init(DMA_CHANNEL_GIF);
 
-  gsGlobal->BGColor->Red = 0x87;
-  gsGlobal->BGColor->Green = 0x47;
-  gsGlobal->BGColor->Blue = 0xbd;
+  // gsGlobal->BGColor->Red = 0x87;
+  // gsGlobal->BGColor->Green = 0x47;
+  // gsGlobal->BGColor->Blue = 0xbd;
 
   gsKit_init_screen(gsGlobal);
 
@@ -32,6 +32,12 @@ int main(int argc, char *argv[]) {
   u64 myColor = GS_SETREG_RGBAQ(0x87,0x47,0xbd,0x00,0x00);
 
   gsKit_clear(gsGlobal, myColor);
+
+  gsKit_mode_switch(gsGlobal, GS_ONESHOT);
+  
+  gsKit_queue_exec(gsGlobal);
+
+  gsKit_sync_flip(gsGlobal);
 
   gsKit_deinit_global(gsGlobal);
 
